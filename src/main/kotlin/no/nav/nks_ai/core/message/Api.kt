dabcf993aa506441a96157a986778f6d77efd8c2
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2024-10-28 08:11:12 +0100

    re-enable and upgrade ktor-swagger-ui to 4.0.0

--- src/main/kotlin/no/nav/nks_ai/core/message/Api.kt
+++ src/main/kotlin/no/nav/nks_ai/core/message/Api.kt
@@ -1,34 +1,31 @@
 package no.nav.nks_ai.core.message
 
+import io.github.smiley4.ktorswaggerui.dsl.routing.get
+import io.github.smiley4.ktorswaggerui.dsl.routing.post
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.request.receiveNullable
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
-import io.ktor.server.routing.get
-import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 
 fun Route.messageRoutes(messageService: MessageService) {
     route("/messages") {
-        get(
-            "/{id}",
-            /* {
-                       description = "Get a message with the given ID"
-                       request {
-                           pathParameter<String>("id") {
-                               description = "ID of the message"
-                           }
-                       }
-                       response {
-                           HttpStatusCode.OK to {
-                               description = "The operation was successful"
-                               body<Message> {
-                                   description = "The message requested"
-                               }
-                           }
-                       }
-                   } */
-        ) {
+        get("/{id}", {
+            description = "Get a message with the given ID"
+            request {
+                pathParameter<String>("id") {
+                    description = "ID of the message"
+                }
+            }
+            response {
+                HttpStatusCode.OK to {
+                    description = "The operation was successful"
+                    body<Message> {
+                        description = "The message requested"
+                    }
+                }
+            }
+        }) {
             val messageId = call.messageId()
                 ?: return@get call.respond(HttpStatusCode.BadRequest)
 
@@ -39,28 +36,25 @@ fun Route.messageRoutes(messageService: MessageService) {
 
             call.respond(message)
         }
-        post(
-            "/{id}/feedback",
-            /* {
-                       description = "Create a new feedback for a message"
-                       request {
-                           pathParameter<String>("id") {
-                               description = "ID of the message"
-                           }
-                           body<NewFeedback> {
-                               description = "The feedback to be created"
-                           }
-                       }
-                       response {
-                           HttpStatusCode.Created to {
-                               description = "The feedback was created"
-                               body<Feedback> {
-                                   description = "The feedback that got created"
-                               }
-                           }
-                       }
-                   } */
-        ) {
+        post("/{id}/feedback", {
+            description = "Create a new feedback for a message"
+            request {
+                pathParameter<String>("id") {
+                    description = "ID of the message"
+                }
+                body<NewFeedback> {
+                    description = "The feedback to be created"
+                }
+            }
+            response {
+                HttpStatusCode.Created to {
+                    description = "The feedback was created"
+                    body<Feedback> {
+                        description = "The feedback that got created"
+                    }
+                }
+            }
+        }) {
             val messageId = call.messageId()
                 ?: return@post call.respond(HttpStatusCode.BadRequest)
 
