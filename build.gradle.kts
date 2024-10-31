572a1577b854d1d7f059c6605d16b4a0f9b9486e
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2024-10-31 14:08:39 +0100

    upgrade to ktor 3.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val flyway_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.20"
-    id("io.ktor.plugin") version "3.0.0"
+    id("io.ktor.plugin") version "3.0.1"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
     id("com.gradleup.shadow") version "8.3.2"
 }
