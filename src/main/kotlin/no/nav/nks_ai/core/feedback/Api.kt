2c5960284e5e3edc6c217a274a509c3ca9c300c6
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2025-10-10 10:35:11 +0200

    add api docs for sorting

--- src/main/kotlin/no/nav/nks_ai/core/feedback/Api.kt
+++ src/main/kotlin/no/nav/nks_ai/core/feedback/Api.kt
@@ -8,6 +8,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.request.receive
 import io.ktor.server.routing.Route
 import no.nav.nks_ai.app.Page
+import no.nav.nks_ai.app.Sort
 import no.nav.nks_ai.app.pagination
 import no.nav.nks_ai.app.respondEither
 
@@ -27,6 +28,11 @@ fun Route.feedbackAdminRoutes(feedbackService: FeedbackService) {
                 }
                 queryParameter<Int>("size") {
                     description = "How many feedbacks to fetch (default = 100)"
+                    required = false
+                }
+                queryParameter<String>("sort") {
+                    description =
+                        "Sort order (default = ${Sort.CreatedAtDesc.value}). Valid values: ${Sort.validValues}"
                     required = false
                 }
             }
