7a2b94956e8d4246f9e8860370f1f97f034cc139
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2025-12-18 07:51:40 +0100

    add tests for health endpoints

--- src/test/kotlin/no/nav/nks_ai/ApplicationTest.kt
+++ src/test/kotlin/no/nav/nks_ai/ApplicationTest.kt
@@ -1,17 +1,42 @@
 package no.nav.nks_ai
 
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import io.ktor.server.testing.*
-import kotlin.test.*
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.routing.route
+import io.ktor.server.testing.testApplication
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import no.nav.nks_ai.app.plugins.configureMonitoring
+import no.nav.nks_ai.app.plugins.healthRoutes
 
 class ApplicationTest {
-    // @Test
-    // fun testRoot() = testApplication {
-    //     client.get("/").apply {
-    //         assertEquals(HttpStatusCode.OK, status)
-    //         assertEquals("Hello World!", bodyAsText())
-    //     }
-    // }
+    @Test
+    fun testHealth() = testApplication {
+        application {
+            configureMonitoring()
+        }
+
+        routing {
+            route("/internal") {
+                healthRoutes()
+            }
+        }
+
+        client.get("/internal/is_alive").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("alive", bodyAsText())
+        }
+
+        client.get("/internal/is_ready").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("ready", bodyAsText())
+        }
+
+        client.get("/internal/prometheus").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertTrue { bodyAsText().isNotEmpty() }
+        }
+    }
 }
