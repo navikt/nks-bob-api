40a023da11466da5a43c0db23f54cd562522078f
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2026-05-19 15:10:40 +0200

    add texas client

---
+++ shared/src/main/kotlin/no/nav/nks_ai/shared/auth/TexasClient.kt
@@ -0,0 +1,54 @@
+package no.nav.nks_ai.shared.auth
+
+import io.github.oshai.kotlinlogging.KLogger
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.http.isSuccess
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+class TexasClient(
+    private val naisTokenEndpoint: String,
+    private val httpClient: HttpClient,
+    private val logger: KLogger
+) {
+    suspend fun getMachineToken(targetAudience: String): String = createMachineToken(targetAudience).accessToken
+
+    private suspend fun createMachineToken(
+        targetAudience: String
+    ): TexasTokenResponse {
+        val response = httpClient.post(naisTokenEndpoint) {
+            contentType(ContentType.Application.Json)
+            setBody(
+                TexasTokenRequest(
+                    target = targetAudience,
+                    identityProvider = "entra_id"
+                )
+            )
+        }
+
+        if (!response.status.isSuccess()) {
+            logger.error { "Could not fetch machine token: ${response.status.value} (${response.status.description})" }
+            error("Could not fetch machine token: ${response.status.description}")
+        }
+
+        return response.body<TexasTokenResponse>()
+    }
+}
+
+@Serializable
+data class TexasTokenRequest(
+    val target: String,
+    @SerialName("identity_provider") val identityProvider: String,
+)
+
+@Serializable
+data class TexasTokenResponse(
+    @SerialName("access_token") val accessToken: String,
+    @SerialName("expires_in") val expiresIn: Int,
+    @SerialName("token_type") val tokenType: String,
+)
